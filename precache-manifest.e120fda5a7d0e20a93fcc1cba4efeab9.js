df4e9a9447e8aef75ed0cd5ec5e55a21d7c4a52c
Matias Caporale <matiasccyo@gmail.com>
2020-08-10 12:12:07 -0300

    Updates

---
+++ precache-manifest.e120fda5a7d0e20a93fcc1cba4efeab9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc2c0e4c93f949f8153ee182c99fb1f2",
+    "url": "/marcmusicofficial/index.html"
+  },
+  {
+    "revision": "163b8b0da49833a08c85",
+    "url": "/marcmusicofficial/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "e4035c2469cc1f943b6a",
+    "url": "/marcmusicofficial/static/css/main.cdd6b0f9.chunk.css"
+  },
+  {
+    "revision": "163b8b0da49833a08c85",
+    "url": "/marcmusicofficial/static/js/2.ac800ffd.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/marcmusicofficial/static/js/2.ac800ffd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4035c2469cc1f943b6a",
+    "url": "/marcmusicofficial/static/js/main.000c8516.chunk.js"
+  },
+  {
+    "revision": "5eb10ba1f173df4497bd",
+    "url": "/marcmusicofficial/static/js/runtime-main.1a1d0dac.js"
+  },
+  {
+    "revision": "c32d28961b43a619868b00292972e0b5",
+    "url": "/marcmusicofficial/static/media/about.c32d2896.png"
+  },
+  {
+    "revision": "9dd194897b409b11e96ea5e5c80714e4",
+    "url": "/marcmusicofficial/static/media/beach.9dd19489.jpg"
+  },
+  {
+    "revision": "aa71f6f876a8d2ac8fa5f649e59ab7d1",
+    "url": "/marcmusicofficial/static/media/icons8-música-64.aa71f6f8.png"
+  },
+  {
+    "revision": "1ccf7a17addc20e0f3863532244c78ed",
+    "url": "/marcmusicofficial/static/media/landing.1ccf7a17.png"
+  }
+]);
